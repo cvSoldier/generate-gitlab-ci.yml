2ef07c1e32def5c6f0b487f0076e2a61526823b8
cvSolider <610496306@qq.com>
2022-09-18 17:25:47 +0800

    完善questions

--- bin/index.js
+++ bin/index.js
@@ -8,119 +8,56 @@ var inquirer = require('inquirer');
 var chalk = require('chalk');
 const generateFile = require('../src/template.js')
 
-generateFile()
 program
   .version('1.0.0', '-v, --version')
-  .command('create [createname]')
+  .command('create')
   .alias('c')
-  .description('这是一个帮助提示')
-  .option('-a, --modulename [moduleName]', '模块名称')
-  .action((createName) => {
-    console.log('指令 create 后面跟的参数值 createname: ' + createName);
-    // 获得了参数，可以在这里做响应的业务处理
+  .description('根据模版创建.gitlab-ci.yml')
+  .action(() => {
     //用户交互配置，支持多种方式的输入
     var prompList = [
       {
           type:'input',
-          message:'姓名',
-          name:'name'
-      },{
+          message:'测试环境oss地址',
+          name:'ossUat'
+      },
+      {
           type:'input',
-          message:'手机号',
-          name:'phone',
-          validate:val=>{
-              if(val.match(/\d{11}/g)){
-                  return true
-              }
-              return '请输入11位数字'
-          }
-      },{
-          type:'confirm',
-          message:'是否参加测试？',
-          name:'assess',
-          prefix:'前缀'
-      },{
-          type:'confirm',
-          message:'是否同意协议？',
-          name:'notice',
-          suffix:'后缀',
-          when:answers=>{
-              return answers.assess
-          }
-      },{
-          type:'list',
-          message:'请选择学历：',
-          name:'eductionBg',
-          choices:[
-              "大专",
-              "本科",
-              "本科以上"
-          ],
-          filter:val=>{//将选择的内容后面加学历
-              return val+'学历'
-          }
-      },{
-          type:'rawlist',
-          message:'请选择你爱玩的游戏：',
-          name:'game',
-          choices:[
-              "农药",
-              "吃鸡",
-          ]
-      },{
-          type:'expand',
-          message:'请选择你喜欢的水果：',
-          name:'fruit',
-          choices: [
-              {
-                  key: "a",
-                  name: "Apple",
-                  value: "apple"
-              },
-              {
-                  key: "O",
-                  name: "Orange",
-                  value: "orange"
-              },
-              {
-                  key: "p",
-                  name: "Pear",
-                  value: "pear"
-              }
-          ]
-      },{
-          type:'checkbox',
-          message:'请选择你喜欢的颜色：',
-          name:'color',
-          choices:[
-              {
-                  name: "red"
-              },
-              new inquirer.Separator(), // 添加分隔符
-              {
-                  name: "blur",
-                  checked: true // 默认选中
-              },
-              {
-                  name: "green"
-              },
-              new inquirer.Separator("--- 分隔符 ---"), // 自定义分隔符
-              {
-                  name: "yellow"
-              }
-          ]
-      }
+          message:'生产环境oss地址',
+          name:'ossProd'
+      },
+      {
+        type:'confirm',
+        message:'是否需要把结果通知到企业微信(需要企微机器人的webhook key)？',
+        name:'notifyNeed',
+      },
+      {
+        type:'input',
+        message:'webhook key',
+        name:'webhookKey',
+        when: (answers) => answers.notifyNeed
+      },
+      {
+        type:'confirm',
+        message:'是否需要自动化打签？',
+        name:'notifyAutoTag',
+      },
+      {
+        type:'list',
+        message:'选择oss账号：',
+        name:'ossAccount',
+        choices:[
+            "ec",
+            "sx",
+            "cx"
+        ]
+     }
     ]
     //获取用户的输入结果
     inquirer.prompt(prompList).then(answers=>{
       console.log(answers);//用户输入结果
       generateFile()
     })
-  })
-  //自定义帮助信息
-  .on('--help', function () {
-      console.log('把相亲对象组成球队')
-      console.log('去夺下那世界杯')
   })
 
 program.parse(process.argv)
